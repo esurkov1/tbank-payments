94e53986cc79626aedb781f19efe56ccd1f37e4d
esurkov1 <esurkovv@yandex.ru>
2025-11-19 08:59:02 +0300

    chore: подавление ожидаемых ошибок API в интеграционных тестах

--- tests/integration/setup.js
+++ tests/integration/setup.js
@@ -6,3 +6,19 @@ jest.setTimeout(60000);
 
 // Отключаем мокирование axios для реальных запросов
 jest.unmock('axios');
+
+// Подавляем console.error для ожидаемых ошибок в интеграционных тестах
+const originalError = console.error;
+beforeAll(() => {
+  console.error = jest.fn((...args) => {
+    // Показываем ошибки только если они не связаны с T-Bank API
+    const message = args.join(' ');
+    if (!message.includes('[T-Bank]')) {
+      originalError(...args);
+    }
+  });
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
